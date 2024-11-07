f1893704766382115035a1d98e85197dc8c66d01
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2024-11-07 18:19:35 +0300

    Add benchmarks

--- run.sh
+++ run.sh
@@ -5,3 +5,6 @@ rm -f /tmp/fifo && mkfifo /tmp/fifo && (build/bin/learner 1 1 < /tmp/fifo) 1>&2 
 build/bin/learner 1 1 < fifo | RANDOM_SEED=6255 HEIGHT=1 WIDTH=1 python mat/advanced12iq/main.py > fifo
 
 rm -f fifo logs mat.log && mkfifo fifo && build/bin/learner 1 1 < fifo | RANDOM_SEED=6255 HEIGHT=1 WIDTH=1 python mat/advanced12iq/main.py > fifo
+
+hyperfine --warmup 3 --export-markdown "$(git rev-parse --short HEAD).md" 'rm -f fifo logs mat.log && mkfifo fifo && time build/bin/learner 3 3 < fifo | HEIGHT=3 WIDTH=3 RANDOM_SEED=123 python mat/advanced12iq/main.py 2>mat.errlogs > fifo || echo "Done"'
+Benchmark 1: rm -f fifo logs mat.log && mkfifo fifo && time build/bin/learner 3 3 < fifo | HEIGHT=3 WIDTH=3 RANDOM_SEED=123 python mat/advanced12iq/main.py 2>mat.errlogs > fifo || echo "Done"
